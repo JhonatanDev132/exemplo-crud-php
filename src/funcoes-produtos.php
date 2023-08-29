b8e678777c090cc773e27d970d1946e2d4491398
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-08-29 08:40:07 -0300

    função produto feita

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -2,6 +2,16 @@
 require_once "conecta.php";
 
 function lerProdutos(PDO $conexao):array {
+    $sql = "SELECT nome, preco, quantidade FROM produtos ORDER BY nome";
+    try{
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute();
+        $resultado->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $erro) {
+        die("Erro ao carregar produtos: ".$erro->getMessage());
+    }
+
+    return $resultado
     // SQL DE SELECT
     // TRY/CATCH
         // PREPARE...
